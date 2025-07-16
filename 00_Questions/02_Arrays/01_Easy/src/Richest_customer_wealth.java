7958a4bd93bdf8d650ba4a9d17e1e207afe1e238
Abhi Aditya <abhiaditya0755@gmail.com>
2025-07-16 22:12:30 +0530

    question

---
+++ 00_Questions/02_Arrays/01_Easy/src/Richest_customer_wealth.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/richest-customer-wealth/submissions/
+
+public class Richest_customer_wealth {
+    public static void main(String[] args) {
+
+    }
+
+    public int maximumWealth(int[][] accounts) {
+        int ans = Integer.MIN_VALUE;
+        for (int[] person : accounts) {
+            int sum = 0;
+            for (int account : person) {
+                sum += account;
+            }
+            if(sum>ans){
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}
